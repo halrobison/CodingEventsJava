ccc98939ee579876196f42dbe8d62dbeb9ab7c37
Hal's Laptop <halrobisoncreative@gmail.com>
2024-06-04 21:43:50 -0500

    updated eventtype enum to eventcategory many-to-one

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -2,11 +2,8 @@ package org.launchcode.codingevents.controllers;
 
 import jakarta.validation.Valid;
 import org.launchcode.codingevents.data.EventCategoryRepository;
-import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventCategory;
-import org.launchcode.codingevents.models.EventType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
